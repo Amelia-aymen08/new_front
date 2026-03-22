580e23aa4c16029039b5382091afad575a044eb1
Amelia08-14 <a.benelhadj@aymenpromotion.com>
2026-03-22 09:39:47 +0100

    formulaire devis

--- src/pages/ProjectDetailsPage.tsx
+++ src/pages/ProjectDetailsPage.tsx
@@ -563,13 +563,34 @@ function PlansAndLocationWrapper({ plans, location }: { plans: Plan[]; location:
   );
 }
 
+// --- Countries and Wilayas Data ---
+const ALGERIAN_WILAYAS = [
+  "01 - Adrar", "02 - Chlef", "03 - Laghouat", "04 - Oum El Bouaghi", "05 - Batna", 
+  "06 - Béjaïa", "07 - Biskra", "08 - Béchar", "09 - Blida", "10 - Bouira", 
+  "11 - Tamanrasset", "12 - Tébessa", "13 - Tlemcen", "14 - Tiaret", "15 - Tizi Ouzou", 
+  "16 - Alger", "17 - Djelfa", "18 - Jijel", "19 - Sétif", "20 - Saïda", 
+  "21 - Skikda", "22 - Sidi Bel Abbès", "23 - Annaba", "24 - Guelma", "25 - Constantine", 
+  "26 - Médéa", "27 - Mostaganem", "28 - M'Sila", "29 - Mascara", "30 - Ouargla", 
+  "31 - Oran", "32 - El Bayadh", "33 - Illizi", "34 - Bordj Bou Arréridj", "35 - Boumerdès", 
+  "36 - El Tarf", "37 - Tindouf", "38 - Tissemsilt", "39 - El Oued", "40 - Khenchela", 
+  "41 - Souk Ahras", "42 - Tipaza", "43 - Mila", "44 - Aïn Defla", "45 - Naâma", 
+  "46 - Aïn Témouchent", "47 - Ghardaïa", "48 - Relizane", "49 - El M'Ghair", "50 - El Meniaa", 
+  "51 - Ouled Djellal", "52 - Bordj Baji Mokhtar", "53 - Béni Abbès", "54 - Timimoun", 
+  "55 - Touggourt", "56 - Djanet", "57 - In Salah", "58 - In Guezzam"
+];
+
+const COUNTRIES = [
+  "Algérie", "France", "Canada", "Belgique", "Suisse", "Tunisie", "Maroc", "Espagne", 
+  "Italie", "Allemagne", "États-Unis", "Royaume-Uni", "Turquie", "Émirats Arabes Unis", "Qatar", "Autre"
+];
+
 function DetailsContact({ projectTitle }: { projectTitle?: string }) {
   const [formData, setFormData] = useState({
     email: "",
     lastName: "",
     firstName: "",
     phone: "",
-    country: "",
+    country: "Algérie",
     wilaya: "",
     budget: "",
     profession: "",
@@ -595,7 +616,14 @@ function DetailsContact({ projectTitle }: { projectTitle?: string }) {
             setFormData(prev => ({ ...prev, consent: checked }));
         }
     } else {
-        setFormData(prev => ({ ...prev, [name]: value }));
+        setFormData(prev => {
+          const newData = { ...prev, [name]: value };
+          // Clear wilaya if country is not Algeria
+          if (name === 'country' && value !== "Algérie") {
+            newData.wilaya = "";
+          }
+          return newData;
+        });
     }
   };
 
@@ -705,8 +733,10 @@ function DetailsContact({ projectTitle }: { projectTitle?: string }) {
               </div>
             </div>
 
-            <SelectGroup label="Pays" name="country" value={formData.country} onChange={handleChange} options={["Algeria", "France", "Canada", "Autre"]} />
-            <SelectGroup label="Wilaya *" name="wilaya" value={formData.wilaya} onChange={handleChange} options={["Alger", "Oran", "Constantine", "Autre"]} required />
+            <SelectGroup label="Pays" name="country" value={formData.country} onChange={handleChange} options={COUNTRIES} />
+            {formData.country === "Algérie" && (
+              <SelectGroup label="Wilaya *" name="wilaya" value={formData.wilaya} onChange={handleChange} options={ALGERIAN_WILAYAS} required />
+            )}
             <SelectGroup label="Budget estimé *" name="budget" value={formData.budget} onChange={handleChange} options={["< 20M DA", "20M DA - 40M DA", "> 40M DA"]} required />
             <SelectGroup label="Secteur d'activité *" name="profession" value={formData.profession} onChange={handleChange} options={["Salarié", "Profession Libérale", "Commerçant", "Autre"]} required />
             <SelectGroup label="Type de financement *" name="financing" value={formData.financing} onChange={handleChange} options={["Fonds Propres", "Crédit Bancaire", "Mixte"]} required />
