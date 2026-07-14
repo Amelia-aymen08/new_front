a1e5dc059ff0a35e3b1e9eb25fc014564b7a737a
Amelia08-14 <a.benelhadj@aymenpromotion.com>
2026-07-14 09:11:06 +0100

    seo correction 8

--- src/pages/AymagPage.tsx
+++ src/pages/AymagPage.tsx
@@ -5,6 +5,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Seo from "../components/Seo";
 
 // Pour éviter l'erreur "Cannot access uninitialized variable" ou des soucis de lazy loading avec react-pageflip
 import HTMLFlipBook from "react-pageflip";
